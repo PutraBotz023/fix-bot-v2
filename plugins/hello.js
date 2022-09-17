19cabae9624372db03b1328b26e7f19088bdec05
GansKatriel <112304004+GansKatriel@users.noreply.github.com>
2022-09-17 09:44:47 +0800

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -9,7 +9,7 @@ conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply
     mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
     mediaType: 2,
     description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
-    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    title: 'ᴋʜᴜʀᴜsʜɪ-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig  }}})

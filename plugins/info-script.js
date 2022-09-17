9b7c91d1ec1142d5330cd8268467ec468387a425
GansKatriel <112304004+GansKatriel@users.noreply.github.com>
2022-09-17 09:48:37 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,26 +2,21 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*hak cipta milik WH MODS MODDER*
 
-Pengen mentahan nya ?
-Nih ada santai bro....
+BASE SCRIPT YANG DI RECODE RIELL :
 https://github.com/ImYanXiao/Elaina-MultiDevice
 
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtu.be/Pgv_GuQTPyo
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6282127487538`
+script recode by Riell: private deks🐤
+*Buy di owner Riell DAN THANKS KEPADA WH MODS MODDER YANG TELAH MEMBANTU RECODE SCRIPT INI*
+wa.me/6282146218274 (RIELGANZZ)`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/_ctzhid",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'RIELFURR-DEV',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
